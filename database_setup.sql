45481ea2fa2e8782f3b53f821b456e13ee6afb76
Muawwidh <160059450+muawwidh@users.noreply.github.com>
2025-01-23 01:03:02 +0200

    Create database_setup.sql
    This consists of the details of the 12 products that are needed to be added to the database

---
+++ database_setup.sql
@@ -0,0 +1,130 @@
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`, `additional_images`,
+`category`) VALUES (NULL, 'Smart Trash Bin', '399.99', 'Intelligent and interactive motion sensor for
+hands-free operation. A more efficient and environment safe method of waste disposal. Hands-free
+motion sensor operation with soft close, gently and quietly ease it down. Removable ring liner holds
+the trash bag sturdy in place and prevents the bag from overhanging for a neat and clean
+appearance. Use any standard trash bags. Delay sensing technology preserves battery life and
+prevents any unnecessary opening from pets, children, or walking by.', 'img/Smart Trash
+Bin/51JomWRA0aL._SL1500_.jpg', 'img/Smart Trash Bin/51JomWRA0aL._SL1500_.jpg,img/Smart
+Trash Bin/base.jpg, img/Smart Trash Bin/sensor lid.jpg, img/Smart Trash Bin/lid.jpg', 'home')
+
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`, `additional_images`,
+`category`) VALUES (NULL, 'Electric Sit Stand Desk', '849.99', 'Foxby Tech\'s electric sit-stand desk
+offers easy transitions, a spacious surface, and sturdy build for ergonomic and durable workspace.
+Switch between sitting and standing with ease on the Foxby Tech sit-stand desk, promoting health
+and productivity with its sturdy and spacious design.', 'img/Standing Desk/1.jpg', 'img/Standing
+Desk/1.jpg,img/Standing Desk/3.jpg, img/Standing Desk/4.jpg, img/Standing Desk/5.jpg', 'home')
+
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`, `additional_images`,
+`category`) VALUES (NULL, 'Moving Gear Wall Clock', '350.00', 'Elegantly aged vintage copper
+clock, meticulously handcrafted. Intricate details shine through verdigris patina. A timeless
+masterpiece merging artistry and function. Discover a mesmerizing moving gear clock, blending art
+and timekeeping. Elegant design, rotating gears, and precise motion create a captivating visual
+experience for any space. Large wall clock with unique moving parts is as attractive as it is
+functional.', 'img/Wall Clock/1.jpg', 'img/Wall Clock/1.jpg,img/Wall Clock/2.jpg, img/Wall
+Clock/3.jpg, img/Wall Clock/4.jpg', 'home')
+
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`, `additional_images`,
+`category`) VALUES (NULL, 'Dual Air Fryer', '550.00', 'The Large 12L Digital Air Fryer boasts a sleek
+design with an aluminum and die-cast finish, combining durability and style for your kitchen. Enjoy
+healthy, delicious meals with its advanced cooking technology. The Large Non-Stick Pan for your air
+fryer ensures easy, mess-free cooking. Its non-stick surface prevents food from sticking and
+simplifies cleaning, making it an essential accessory for your air frying needs. Our premium
+packaging for the air fryer offers superior protection and an elegant presentation. Designed to
+safeguard your investment during shipping, it adds a touch of sophistication to your purchase
+experience.', 'img/Air Fryer/1.jpg', 'img/Air Fryer/1.jpg,img/Air Fryer/2.jpg, img/Air Fryer/3.jpg, img/Air
+Fryer/4.jpg', 'home')
+
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`, `additional_images`,
+`category`) VALUES (NULL, 'Ambient Splice Lights', '249.99', '360° IR Remote Controller. We use the
+latest arc-shaped remote control that feels more comfortable, omni-directional control without
+dead angle, bring you a different experience. Updated-Convenient Controller Box. 3-buttons control
+box is for your convenience to simply turn on/off, change color and dynamic mode.We have
+updated the controller box to not be interfered by other devices. Smart APP Control-\"Tuya\".
+Connect to Alexa Google Home and also there are 16 million colors, 1%-100% brightness, preprogrammed style modes, music mode, time and memory function, you can freely control the RGB
+LED lamp by your phone..', 'img/Ambient Splice Lights/11.jpg', 'img/Ambient Splice
+Lights/11.jpg,img/Ambient Splice Lights/33.jpg,img/Ambient Splice Lights/22.jpg,img/Ambient
+Splice Lights/44.jpg', 'lights')
+
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`, `additional_images`,
+`category`) VALUES (NULL, '1.2m LED Lights', '199.99', '60° IR Remote Controlle. We use the latest
+arc-shaped remote control that feels more comfortable, omni-directional control without dead
+angle, bring you a different experience.Updated-Convenient Controller Box. 3-buttons control box is
+for your convenience to simply turn on/off, change color and dynamic mode.We have updated the
+controller box to not be interfered by other devices.Smart APP Control-\"Tuya\". Connect to Alexa
+Google Home and also there are 16 million colors, 1%-100% brightness, pre-programmed style
+modes, music mode, time and memory function, you can freely control the RGB LED lamp by your
+phone.', 'img/1.2m LED light/1.jpg', 'img/1.2m LED light/1.jpg,img/1.2m LED light/2.jpg,img/1.2m
+LED light/3.jpg,img/1.2m LED light/5.jpg', 'lights')
+
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`, `additional_images`,
+`category`) VALUES (NULL, 'Hexagonal Lights', '149.99', ' Use remote controls to change color of
+Hexagon lights. Unique USB power supply, can be connected to the power bank, computer,
+notebook, so you can use in multiple scenarios. This is a modular touch-sensing wall light. Use
+hexagonal lights to create any structure to suit your needs,13 fixed colors and 3 dynamic modes
+that are switchable, and turn it on and off by touching it. The minimalist design and unique
+geometric shape not only illuminate but stylize your home. Rearrange light combination through
+connecting pieces. Can be placed in the living room, bedroom, study, restaurant, hotel, etc.
+Technical specifications: Six USB ports per product (power can be input to any of the 6 sides) 13
+Blend Colors, 5 mode settings: Color, Gradient, Transition, Flash, Timing. 21-key remote control:
+Remote control distance: 16-25 feet, 13-color remote control, 4 modes plus timing function
+SMD5050 RGB led lights that produce 240 Lumens Comes with sticky pads that can be affixed to
+most surfaces. Timing function: It can be turned off automatically after 30 minutes.', 'img/Hexagon
+Lights/5.jpg', 'img/Hexagon Lights/5.jpg,img/Hexagon Lights/3.jpg,img/Hexagon
+Lights/4.jpg,img/Hexagon Lights/8.jpg', 'lights')
+
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`, `additional_images`,
+`category`) VALUES (NULL, 'Triangle Lights', '299.99', '360° IR Remote Controller. We use the latest
+arc-shaped remote control that feels more comfortable, omni-directional control without dead
+angle, bring you a different experience. Updated-Convenient Controller Box. 3-buttons control box
+is for your convenience to simply turn on/off, change color and dynamic mode.We have updated the
+controller box to not be interfered by other devices. Smart APP Control-\"Tuya\". Connect to Alexa
+Google Home and also there are 16 million colors, 1%-100% brightness, pre-programmed style
+modes, music mode, time and memory function, you can freely control the RGB LED lamp by your
+phone.', 'img/Triangle Lights/8.jpg', 'img/Triangle Lights/8.jpg,img/Triangle Lights/7.jpg,img/Triangle
+Lights/4.jpg,img/Triangle Lights/2.jpg', 'lights')
+
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`, `additional_images`,
+`category`) VALUES (NULL, 'RGB Gaming Mouse Pad', '39.99', 'This innovative mouse pad not only
+boasts a large surface area for smooth mouse movement, but it also features vibrant RGB lighting
+to elevate your gaming setup to the next level. With its USB cable connectivity, you can easily plug
+in and start using the mouse pad without any additional setup. Plus, the cable is detachable for
+easy storage and transportation. This mouse pad is also designed to withstand the rigors of intense
+gaming sessions, as it is completely waterproof. The RGB lighting can be customized to your
+preferences with a variety of colors and effects to choose from, allowing you to personalize your
+setup to match your gaming style. The lighting is powered by USB, ensuring that it won\'t drain your
+device\'s battery.', 'img/MousePad/6.jpg',
+'img/MousePad/6.jpg,img/MousePad/4.jpg,img/MousePad/3.jpg,img/MousePad/1.jpg',
+'electronics')
+
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`,
+`additional_images`, `category`) VALUES (NULL, 'Adjustable Dual Monitor Desk Stand', '89.99', 'A
+dual monitor arm with an adjustable height feature can accommodate different users needs and
+preferences. This feature can also help prevent neck and eye strain by allowing you to position your
+monitors at the appropriate height. Being able to swivel and tilt your monitors can help you achieve
+the perfect viewing angle, especially if you need to share your screen with others or if you work with
+multiple applications simultaneously.', 'img/Monitor Stand/4.jpg', 'img/Monitor
+Stand/4.jpg,img/Monitor Stand/2.jpg,img/Monitor Stand/6.jpg,img/Monitor Stand/1.jpg',
+'electronics')
+
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`,
+`additional_images`, `category`) VALUES (NULL, '5 Plug Extension Socket', '99.99', 'Looking for a
+reliable power strip surge protector that can keep your devices safe from sudden voltage spikes and
+lightning strikes? Look no further than the New Trend UK Power Strip Surge Protector! With 5 AC
+outlets and multiple USB ports, this power strip can handle all your charging needs. It also features
+a PD 20w USB C output, 12W USB A total output, and 2.4A high-current delivery for each USB port.
+The soft LED indicator and switch controller make it easy to use, while the 5ft cord gives you plenty
+of room to maneuver. Perfect for home or office use, this tabletop extension socket is a must-have
+for anyone who wants to protect their devices from harm.', 'img/5Socket/1.jpg',
+'img/5Socket/1.jpg,img/5Socket/2.jpg,img/5Socket/3.jpg,img/5Socket/4.jpg', 'electronics')
+
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`,
+`additional_images`, `category`) VALUES (NULL, '9 Plug Extension Socket', '129.99', 'Looking for a
+reliable power strip surge protector that can keep your devices safe from sudden voltage spikes and
+lightning strikes? Look no further than the New Trend UK Power Strip Surge Protector! With 9 AC
+outlets and multiple USB ports, this power strip can handle all your charging needs. It also features
+a PD 20w USB C output, 12W USB A total output, and 2.4A high-current delivery for each USB port.
+The soft LED indicator and switch controller make it easy to use, while the 5ft cord gives you plenty
+of room to maneuver. Perfect for home or office use, this tabletop extension socket is a must-have
+for anyone who wants to protect their devices from harm.', 'img/9Socket/1.jpg',
+'img/9Socket/1.jpg,img/9Socket/2.jpg,img/9Socket/3.jpg,img/9Socket/4.jpg', 'electronics')
